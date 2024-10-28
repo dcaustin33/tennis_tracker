63731e4f336819ff2d3153d20843905601a1f4b8
dcaustin33 <dcaustin33@gmail.com>
2024-10-28 17:16:42 -0400

    Changed name and added yellow tennis ball

--- tennis_tracker/ball_tracking/label_with_balls.py
+++ tennis_tracker/ball_tracking/label_with_balls.py
@@ -3,26 +3,27 @@ import os
 import cv2
 import numpy as np
 import torch
+from groundingdino.util.inference import batch_predict, load_image, load_model
 from tqdm import tqdm
-from groundingdino.util.inference import (
-    annotate,
-    batch_predict,
-    load_image,
-    load_model,
-    predict,
-)
-from tennis_tracker.player_location.homography import transform_points
 
 from tennis_tracker.download_data.extract_keypoints import (
     read_json_file,
     write_to_json_file,
 )
+from tennis_tracker.player_location.homography import transform_points
+
 
 def output_point(m: np.array, points: np.array) -> list:
     """points should be in shape (-1, 1, 2)"""
     outputs = cv2.perspectiveTransform(points, m)
     # output will be -1, 1, 2
     return outputs.reshape(-1, 2)
+
+def cut_image_into_quarters(image: np.array) -> list:
+    height = (image.shape[1] // 2) * 2
+    width = (image.shape[2] // 2) * 2
+    image = image[:, :height, :width]
+    return [image[:, :height//2, :width//2], image[:, :height//2, width//2:], image[:, height//2:, :width//2], image[:, height//2:, width//2:]]
 
 if __name__ == "__main__":
 
@@ -33,18 +34,18 @@ if __name__ == "__main__":
     # model = torch.compile(model)
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model = model.to(device)
-    TEXT_PROMPT = "tennis player"
+    TEXT_PROMPT = "yellow tennis ball"
     BOX_TRESHOLD = 0.35
     TEXT_TRESHOLD = 0.25
     JSON_PATH = (
-        "/home/da2986/tennis_tracker/tennis_tracker/download_data/labels.json"
+        "/home/da2986/tennis_tracker/tennis_tracker/psudeo_label/clean_labels.json"
     )
 
     data = read_json_file(JSON_PATH)
     img_paths = [img_path for img_path in data.keys()]
 
     batch_size = 10
-    OUTPUT_JSON_PATH = "/home/da2986/tennis_tracker/tennis_tracker/psudeo_label/labels.json"
+    OUTPUT_JSON_PATH = "/home/da2986/tennis_tracker/tennis_tracker/ball_tracking/labels.json"
     
     if os.path.exists(OUTPUT_JSON_PATH):
         os.remove(OUTPUT_JSON_PATH)
@@ -54,7 +55,8 @@ if __name__ == "__main__":
         loaded_images = []
         for image in batch_images:
             image_source, image = load_image(image)
-            loaded_images.append(image)
+            quarters = cut_image_into_quarters(image)
+            loaded_images.extend(quarters)
         input_images = torch.stack(loaded_images)
         boxes, logits, boxes_to_im = batch_predict(
             model=model,
@@ -71,14 +73,14 @@ if __name__ == "__main__":
             all_boxes = []
             for box in im_boxes:
                 all_boxes.append(f"0 {box[0]} {box[1]} {box[2]} {box[3]}")
-            data[batch_images[im_num]]['boxes'] = all_boxes
+            data[batch_images[im_num]]['ball_tracking_boxes'] = all_boxes
             lines.append(all_boxes)
             
             # now we translate to the world coords
             image_dims = data[batch_images[im_num]]['image_dims'].copy()
             m = np.array(data[batch_images[im_num]]['m'].copy())
             transformed_points = transform_points(m, im_boxes, image_dims)
-            data[batch_images[im_num]]['transformed_coords'] = transformed_points
+            data[batch_images[im_num]]['ball_tracking_transformed_coords'] = transformed_points
             
             
     write_to_json_file(OUTPUT_JSON_PATH, data)
